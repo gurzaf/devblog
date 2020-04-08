f1c691e82173f4003462c93efe2b755c7dd1cb04
julian@soluciones404.com.co <PAJA__R1T02>
2020-04-07 21:55:15 -0500

    New configuration

--- src/cms/preview-templates/blog-post-preview.js
+++ src/cms/preview-templates/blog-post-preview.js
@@ -3,6 +3,9 @@ import PropTypes from "prop-types";
 import { BlogPostTemplate } from "../../templates/blog-post";
 
 const BlogPostPreview = ({ entry }) => {
+  console.warn('===?');
+  console.warn(entry.getIn(["data", "html"]));
+  console.warn('===');
   return (
     <BlogPostTemplate
       post={entry.getIn(["data", "markdownRemark"])}
